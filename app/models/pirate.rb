eebc1194ac1c05022d00c655c756b159f2231c9c
deber116 <deber116@gmail.com>
2020-03-10 16:09:55 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(params)
+        self.name = params[:pirate][:name]
+        self.weight = params[:pirate][:weight]
+        self.height = params[:pirate][:height]
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
 end
